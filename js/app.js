f7bb1dcc62724cd9d2d379f41a3f971688ba6a94
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2022-03-09 14:57:55 +0600

    cardsbox

--- js/app.js
+++ js/app.js
@@ -1,3 +1,21 @@
+
+
+
+// menu toggle
+let toggle=document.querySelector('.toggle');
+let navigation = document.querySelector('.navigation');
+let main =document.querySelector('.main'); 
+
+
+toggle.onclick =function(){
+navigation.classList.toggle('active');
+main.classList.toggle('active');
+}
+
+
+
+
+//add hovered class in selected list item
 let = document.querySelectorAll(".navigation li");
 function activeLink() {
   list.forEach((element) => element.classList.remove("hovered"));
